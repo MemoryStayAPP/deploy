e9327e226c6d9c710ef53fb4ba46fb90348ceedc
Igor Żurawski <87831311+Sirdolorem@users.noreply.github.com>
2023-03-02 00:49:13 +0100

    s

--- clone_repo.sh
+++ clone_repo.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
 rm -rfd laravel
 rm -rfd MemoryStay
 
@@ -8,4 +10,20 @@ git clone https://github.com/MemoryStayAPP/MemoryStay.git
 mv laravel/Memory_Stay .
 rm -rfd laravel
 mv Memory_Stay laravel
+
+cd laravel
+
+cp .env.example .env
+
+composer update
+./vendor/bin/sail up -d
+cd ../MemoryStay/src
+npm install
+cd ..
+npm run build
+docker-compose up -d
+cd ..
+docker-compose up -d
+
+
 
