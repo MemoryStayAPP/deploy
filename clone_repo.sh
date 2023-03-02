5813735ba8c2fedf182a042303b4f85fb773bf1b
Igor Żurawski <87831311+Sirdolorem@users.noreply.github.com>
2023-03-02 01:33:47 +0100

    1

--- clone_repo.sh
+++ clone_repo.sh
@@ -1,29 +1,21 @@
 #! /bin/sh
-docker stop $(docker ps -aq)
-docker rm $(docker ps -aq)
-rm -rfd laravel
-rm -rfd MemoryStay
-
-git clone https://github.com/MemoryStayAPP/laravel.git
-git clone https://github.com/MemoryStayAPP/MemoryStay.git
-
-mv laravel/Memory_Stay .
-rm -rfd laravel
-mv Memory_Stay laravel
-
-cd laravel
-
-cp .env.example .env
-
-composer update
-./vendor/bin/sail up -d
-cd ../MemoryStay/src
-npm install
-cd ..
-npm run build
-docker-compose up -d
-cd ..
-docker-compose up -d
-
-
-
+docker stop $(docker ps -aq);
+docker rm $(docker ps -aq);
+rm -rfd laravel;
+rm -rfd MemoryStay;
+git clone https://github.com/MemoryStayAPP/laravel.git;
+git clone https://github.com/MemoryStayAPP/MemoryStay.git;
+mv laravel/Memory_Stay .;
+rm -rfd laravel;
+mv Memory_Stay laravel;
+cd laravel;
+cp .env.example .env;
+composer update;
+./vendor/bin/sail up -d;
+cd ../MemoryStay/src;
+npm install;
+cd ..;
+npm run build;
+docker-compose up -d;
+cd ..;
+docker-compose up -d;
