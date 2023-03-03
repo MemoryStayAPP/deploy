ecc075a6b765a3b3d87b500d01262150baec485c
Igor Żurawski <87831311+Sirdolorem@users.noreply.github.com>
2023-03-03 14:37:59 +0100

    Update clone_repo.sh

--- clone_repo.sh
+++ clone_repo.sh
@@ -18,6 +18,7 @@ cd ../MemoryStay/src;
 npm install;
 cd ..;
 npm run build;
+docker-compose up -d;
 #docker run -d --name docker-nginx --label "traefik.http.routers.nginx.rule=Host(`app.memorystay.pl`)" -v ./build:/usr/share/nginx/html -p 8081:80 nginx;
 cd ..;
 docker-compose up -d;
